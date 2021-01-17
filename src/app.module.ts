be195fc5df3f6f22d2e6e2386e5ef4c57436043c
Japhet Sebastian <japhet.sebastian01@gmail.com>
2021-01-17 20:31:55 +0300

    initial modules templates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostsModule } from './modules/posts/posts.module';
+import { CommentsModule } from './modules/comments/comments.module';
+import { RepliesModule } from './modules/replies/replies.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PostsModule, CommentsModule, RepliesModule, UsersModule],
 })
 export class AppModule {}
