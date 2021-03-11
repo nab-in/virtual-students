6be18fb5dbb264d161370ce4e4340be7499fdccb
Bennett <bennybenester@gmail.com>
2021-03-11 09:52:37 +0300

    add bash file to initialize the database in docker

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:15.5.1-alpine3.12
 RUN mkdir /home/app
 WORKDIR /home/app
 COPY package.json /home/app/package.json
+COPY files /home/app/files
 COPY ["tsconfig.json", "tsconfig.build.json","./"]
 RUN npm install
 ENV GENERATE_SOURCEMAP=false
--- init-student-db.sh
+++ init-student-db.sh
@@ -1,6 +1,7 @@
 docker container stop student-api
+echo 'The API Container has been stopped'
 docker exec student-db psql -h localhost -U postgres -c "DROP DATABASE IF EXISTS virtualstudents;"
 docker exec student-db psql -h localhost -U postgres -c "CREATE DATABASE virtualstudents;"
 docker exec student-db psql -h localhost -U postgres -c "GRANT ALL ON DATABASE virtualstudents TO postgres;"
-echo 'Starting the API Container'
+echo 'Restarting the API Container'
 docker container start student-api
